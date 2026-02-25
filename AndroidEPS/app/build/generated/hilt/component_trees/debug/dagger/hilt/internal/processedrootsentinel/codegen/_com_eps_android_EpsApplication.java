ceb7fd3acdeae5b740eba17e43fe059224aacae9
KhamidovKhusniddin <ki770760636@gmail.com>
2026-02-25 08:21:39 +0500

    chore: Clean up project structure, ignore build files and sensitive data

--- AndroidEPS/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_eps_android_EpsApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.eps.android.EpsApplication"
-)
-public final class _com_eps_android_EpsApplication {
-}
